8ecfec52cff8b08a1de14a2f96b490b1f1bc209c
Ezequiel <ezequiel.ifsp@gmail.com>
2014-09-16 14:36:59 -0300

    Initial Commit

---
+++ Lixo-Mestre/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lixo-Mestre
+//
+//  Created by Ezequiel Franca dos Santos on 16/09/14.
+//  Copyright (c) 2014 Ezequiel Franca dos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
