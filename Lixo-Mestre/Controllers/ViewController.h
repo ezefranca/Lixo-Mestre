79dd2cbf0045c09f5200ebd0f748f847d65160ac
gabrielvieira <viera_frifri@hotmail.com>
2014-09-23 17:17:28 -0300

    rwtgert
    ertgert

---
+++ Lixo-Mestre/Controllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lixo-Mestre
+//
+//  Created by GABRIEL VIEIRA on 23/09/14.
+//  Copyright (c) 2014 Ezequiel Franca dos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
